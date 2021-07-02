74965a8ca1b77c63de53b55c5ebc30093d9ded69
Lautaro O. Lamalfa <lautarolamalfa@gmail.com>
2021-07-02 20:54:22 -0300

    Primera entrega2

--- src/components/ItemListContainer/itemListContainer.js
+++ src/components/ItemListContainer/itemListContainer.js
@@ -1,24 +1,55 @@
+
 import {useState, useEffect} from "react"
 import { List } from '../ItemList/itemList'
 import { useParams } from "react-router";
 import Loader from "react-loader-spinner";
-import Data from "../../data/data.json"
 
 export const Container = () => {
   const {id} = useParams()
-  const [catalogo, setCatalogo] = useState()
+  const [catalogo, setCatalogo] = useState([])
 
   console.log(id);
-   
+  
+    const Productos = [
+      {
+        id: '1', 
+        title: 'Sofa', 
+        description: 'Marron', 
+        stock: '10 unidades',
+        price: '$60',
+        categoryId: 'Ambiente'
+      },
+      {
+        id: '2', 
+        title: 'Television', 
+        description: '55 pulgadas', 
+        stock: '10 unidades' ,
+        price: '$120',
+        categoryId: 'Televisores'
+      },
+      {
+        id: '3', 
+        title: "Marvel's The Avengers", 
+        description: 'Blu-ray o en version digital', 
+        stock: '10 unidades',
+        price: '$12',
+        categoryId: 'Peliculas y Series'
+      },
+    ]
+    
+    
     useEffect(() => {
       const nuevaPromesa = new Promise((resolve, rej) => {
+        
         setTimeout(() => {
-          resolve(Data.filter((item) => item.id === id))
+          resolve(Productos)
+          rej()
         }, 2000);
-      });
-        nuevaPromesa.then((item) => {
-          setCatalogo(item);
-      });
+      })
+      nuevaPromesa.then((Productos) => {
+        const detalleProducto = id ? Productos.filter((item) => item.categoryId === id) : Productos 
+          setCatalogo(detalleProducto)
+      })
 // eslint-disable-next-line
     },[id])
     
@@ -31,5 +62,5 @@ export const Container = () => {
         )}
       </>
     )
-}
+  };
   
